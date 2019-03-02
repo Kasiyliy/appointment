892653567febb2ba217f50f15291d67c80ba937f
spider-unknown <beka_97.50@mail.ru>
2019-03-03 03:29:21 +0600

    Css assets

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,7 +10,7 @@
 <meta http-equiv="Content-type"
       content="text/html; charset=utf-8">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all"
+<link href="{{asset('css/fonts.css')}}"
       rel="stylesheet"
       type="text/css"/>
 <link rel="stylesheet"
@@ -26,14 +26,14 @@
 <link rel="stylesheet"
       href="{{ url('quickadmin/css') }}/select2.min.css"/>
 <link rel="stylesheet"
-      href="https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
+      href="{{asset('css/jquery-ui.css')}}">
 <link rel="stylesheet"
-      href="//cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css"/>
+      href="{{asset('css/jquery.dataTables.min.css')}}"/>
 <link rel="stylesheet"
-      href="https://cdn.datatables.net/select/1.2.0/css/select.dataTables.min.css"/>
+      href="{{asset('css/select.dataTables.min.css')}}"/>
 <link rel="stylesheet"
-      href="//cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css"/>
+      href="{{asset('css/buttons.dataTables.min.css')}}"/>
 <link rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.css"/>
+      href="{{asset('css/jquery-ui-timepicker-addon.min.css')}}"/>
 <link rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.standalone.min.css"/>
+      href="{{asset('css/bootstrap-datepicker.standalone.min.css')}}"/>
